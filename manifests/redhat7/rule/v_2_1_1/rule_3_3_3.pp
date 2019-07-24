d70a7dc0fadf010be82313a3c833cf084b908018
mike <mike@ubiquitech.co.za>
2019-07-24 10:46:30 +0200

    Update sysctl declarations for herculesteam-augeasproviders_sysctl

--- manifests/redhat7/rule/v_2_1_1/rule_3_3_3.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_3_3_3.pp
@@ -1,15 +1,44 @@
 # 3.3 IPv6
 # 3.3.3 Ensure IPv6 is disabled (Not Scored)
+# Application of 3.3.3 has changed from v2.1.1. to v2.2.0
 class cis_benchmarks::redhat7::rule::v_2_1_1::rule_3_3_3 {
-$file = '/etc/modprobe.d/CIS.conf'
-include ::cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_1_prereq #creates /etc/modprobe.d
-
-    file_line { '(3.3.3) - Ensure IPv6 is disabled (Not Scored)':
-      ensure => present,
+  $file = '/boot/grub2/grub.cfg'
+  if !defined(File[$file]) {
+    file { "(1.4.1) - ${file} ownership":
+      ensure => file,
       path   => $file,
-      line   => 'options ipv6 disable=1',
-      match  => '^options ipv6 disable',
-      }
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0400',
+    }
+  }
+
+  file_line { '(3.3.3) - Ensure IPv6 is disabled (Not Scored)':
+    ensure => present,
+    path   => $file,
+    line   => 'GRUB_CMDLINE_LINUX="ipv6.disable=1"',
+    match  => '^GRUB_CMDLINE_LINUX.?=".*ipv6.disable=1.*$',
+    replace => false,
+  }
 
 
 } #EOF
+
+
+# $file = '/boot/grub2/grub.cfg'
+# if !defined(File[$file]) {
+#   file { "(1.4.1) - ${file} ownership":
+#     ensure => file,
+#     path   => $file,
+#     owner  => 'root',
+#     group  => 'root',
+#     mode   => '0400',
+#   }
+# }
+# file_line { '(4.1.3) Ensure auditing for processes that start prior to auditd is enabled (Scored)':
+#   ensure  => present,
+#   path    => $file,
+#   line    => 'GRUB_CMDLINE_LINUX = "audit=1"',
+#   match   => '^GRUB_CMDLINE_LINUX.?=".*audit=1.*$',
+#   replace => false,
+# }
