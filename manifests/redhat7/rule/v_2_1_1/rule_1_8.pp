76e9b4a3683fa54fe80efcb41b92723729fd6ecf
mike <mike@ubiquitech.co.za>
2019-07-24 21:51:05 +0200

    Update rule 4.1.3 to use herculesteam/augeasproviders_grub package to set values.

--- manifests/redhat7/rule/v_2_1_1/rule_1_8.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_1_8.pp
@@ -1,4 +1,4 @@
-# 1.8 Ensure updates, patches, and additional security software are installed (Not Scored)
+# 1.8 Ensure updates, patches, and additional security software are installed (Scored in v 2.2.0)
 class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_8 {
   $updates = $::cis_benchmarks['yum_updates']
   if $updates == 'yum updates are available'
